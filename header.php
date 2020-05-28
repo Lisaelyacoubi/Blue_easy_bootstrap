5eca47690d4da76840f4ed541201cfff4961794a
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-05-28 16:50:52 +0200

    navbar

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Blue Easy Theme</title>
+
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- FAVICON -->
+  <link rel="icon" type="image/x-icon" href="https://img.icons8.com/wired/64/000000/java-coffee-cup-logo.png">
+
+  <!-- BOOTSTRAP-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <!-- CSS THEME -->
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <header>
+    <!--Navbar start -->
+    <section>
+      <div class="container-fluid">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark-grey padding-navbar">
+            <div class="row">
+              <div class="col-lg-6">
+                <a class="navbar-brand text-uppercase font-weight-bold margin-right-navbar" href="#">Blu<span class="text-info font-weight-bold">e</span>asy</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              </div>
+              <div class="collapse navbar-collapse col-lg-6" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item active">
+                    <a class="nav-link text-uppercase mr-4" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link text-uppercase mr-4" href="#">Services<span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link text-uppercase mr-4" href="#">Features <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link text-uppercase mr-4" href="#">Portfolio <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link text-uppercase" href="#">Contact <span class="sr-only">(current)</span></a>
+                  </li>
+                </ul>
+              </div>
+              <!-- Fin Div row -->
+            </div>
+          </nav>
+          <!-- Navbar end -->
+
+
+        </div>
+      </section>
+  </header>
