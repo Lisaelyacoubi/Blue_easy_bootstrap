5262764f709f9875be4ec3733bc084e4a6a4d07d
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-05-29 10:40:21 +0200

    banner

--- header.php
+++ header.php
@@ -19,17 +19,14 @@
 <body>
   <header>
     <!--Navbar start -->
-    <section>
-      <div class="container-fluid">
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark-grey padding-navbar">
-            <div class="row">
-              <div class="col-lg-6">
-                <a class="navbar-brand text-uppercase font-weight-bold margin-right-navbar" href="#">Blu<span class="text-info font-weight-bold">e</span>asy</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-              </div>
-              <div class="collapse navbar-collapse col-lg-6" id="navbarNav">
+    <section class="banner width">
+      <div class="banner width">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark-grey padding-navbar width">
+              <a class="navbar-brand text-uppercase font-weight-bold margin-right-navbar" href="#">Blu<span class="color-light-blue font-weight-bold">e</span>asy</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse margin-right-left d-flex justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item active">
                     <a class="nav-link text-uppercase mr-4" href="#">Home <span class="sr-only">(current)</span></a>
@@ -44,16 +41,12 @@
                     <a class="nav-link text-uppercase mr-4" href="#">Portfolio <span class="sr-only">(current)</span></a>
                   </li>
                   <li class="nav-item active">
-                    <a class="nav-link text-uppercase" href="#">Contact <span class="sr-only">(current)</span></a>
+                    <a class="nav-link text-uppercase margin-right-contact" href="#">Contact <span class="sr-only">(current)</span></a>
                   </li>
                 </ul>
               </div>
-              <!-- Fin Div row -->
-            </div>
           </nav>
           <!-- Navbar end -->
-
-
         </div>
       </section>
   </header>
