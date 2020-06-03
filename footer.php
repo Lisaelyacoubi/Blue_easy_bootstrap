abd5505c679df6e6f40e0bf7e8487232008dd90c
lisaelyacoubi <lisa.elyacoubi@gmail.com>
2020-06-03 10:15:06 +0200

    footer

--- footer.php
+++ footer.php
@@ -2,20 +2,20 @@
   <!-- Footer start -->
   <section>
     <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-dark ">
+      <nav class="navbar navbar-expand-lg navbar-dark font">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link text-uppercase text-dark-grey " href="#">Home <span class="sr-only">(current)</span> | </a>
+              <a class="nav-link text-uppercase text-dark-grey " href="#">Home <span class="sr-only">(current)</span> <span class="ml-1">|</span> </a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link text-uppercase text-dark-grey" href="#">Services<span class="sr-only">(current)</span> | </a>
+              <a class="nav-link text-uppercase text-dark-grey" href="#">Services<span class="sr-only">(current)</span> <span class="ml-1">|</span>  </a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link text-uppercase text-dark-grey" href="#">Features <span class="sr-only">(current)</span> | </a>
+              <a class="nav-link text-uppercase text-dark-grey" href="#">Features <span class="sr-only">(current)</span> <span class="ml-1">|</span>  </a>
             </li>
             <li class="nav-item active">
               <a class="nav-link text-uppercase text-dark-grey" href="#">Portfolio <span class="sr-only">(current)</span> | </a>
